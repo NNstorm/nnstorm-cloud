ecce6e03aacf98657edfaa9f247021d6e75f50c0
Geza Velkey <evelkey@gmail.com>
2020-11-14 18:16:31 +0100

    Improved readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# NNstorm cloud
+
+## Install
+
+Install release:
+
+```bash
+pip install nnstorm-cloud
+```
+
+## Develop
+
+```bash
+git clone https://github.com/NNstorm/nnstorm-cloud.git
+cd nnstorm-cloud
+pip install -e .
+```
--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ import setuptools
 
 VERSION = "0.4.1"
 
+with open("requirements.txt", "r") as f:
+    reqs = [l.replace("\n", "") for l in f.readlines()]
+
+
 setuptools.setup(
     name="nnstorm-cloud",
     version=VERSION,
@@ -17,5 +21,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
+    install_requires=reqs,
     python_requires=">=3.7",
 )
